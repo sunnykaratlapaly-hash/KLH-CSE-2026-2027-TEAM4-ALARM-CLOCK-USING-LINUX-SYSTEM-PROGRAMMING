88451c0e24454479509836826b4c0a71eed199e3
mailingtarunvarakala-star <mailingtarunvarakala@gmail.com>
2026-08-25 08:38:22 +0530

    Add files via upload

---
+++ Skill/2520030526/Skill2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char input[100];
+
+    while (1)
+    {
+        printf("myshell> ");
+        fflush(stdout);
+
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            printf("\nExiting...\n");
+            break;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0)
+        {
+            printf("Exiting shell...\n");
+            break;
+        }
+
+        printf("You entered: %s\n", input);
+    }
+
+    return 0;
+}
